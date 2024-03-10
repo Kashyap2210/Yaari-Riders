07627d675c13b7d7dad65ddc5b8d8c3c300ad188
Kashyap 2210 <kash.cdac@gmail.com>
2024-03-10 20:04:20 +0530

    Seperate files created for each and every ride

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let fileToBeOpened = document.querySelectorAll("li");
 
 fileToBeOpened.forEach(function (file) {
   file.addEventListener("click", function () {
-    console.log("File is clicked");
+    listItemClicked = file.textContent;
+    window.location = `${listItemClicked}.html`;
   });
 });
