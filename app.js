9fa402064af68a93f968c1afa497c73a46370727
Kashyap 2210 <kash.cdac@gmail.com>
2024-03-14 17:10:56 +0530

    Copy to clipboard feature added on Email Adress

--- app.js
+++ app.js
@@ -12,3 +12,15 @@ let instaRedirect = document.querySelector(".instagram");
 instaRedirect.addEventListener("click", () => {
   window.open("https://www.instagram.com/yaari_riders/", "_blank");
 });
+
+const maidIdCopied = document.querySelector(".for-copy");
+
+maidIdCopied.addEventListener("click", async () => {
+  const maidIdCopy = maidIdCopied.innerHTML;
+  try {
+    await navigator.clipboard.writeText(maidIdCopy);
+    console.log("Text Copied");
+  } catch (error) {
+    console.log("Error while copying text.", error);
+  }
+});
